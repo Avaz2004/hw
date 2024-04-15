5f8b67b7e4bf62aa1d5f252b6ccd0f0fcb4e1e47
Avaz <zsavva00@gmail.com>
2024-04-15 18:57:49 +0700

    c

---
+++ ins.sql
@@ -0,0 +1,98 @@
+insert into Track (name,album_id,duration) 
+values ("Stargazer",1,507);
+
+insert into Track (name,album_id,duration) 
+values ("Dead or alive",2,282);
+
+insert into Track (name,album_id,duration) 
+values ("Hold on",3,310);
+
+insert into Track (name,album_id,duration) 
+values ("Dogs",4,397);
+
+insert into Track (name,album_id,duration) 
+values ("Aces high",5,272);
+
+insert into Track (name,album_id,duration) 
+values ("My dear father",6,441);
+
+insert into Track (name,album_id,duration) 
+values ("Orchid",7,92);
+
+
+insert into Executor (name) 
+values ("Deep purple");
+
+insert into Executor (name) 
+values ("Pink Floyd");
+
+insert into Executor (name) 
+values ("Dio");
+
+insert into Executor (name) 
+values ("Black sabbath");
+
+insert into Executor (name) 
+values ("Iron maiden");
+
+insert into Executor (name) 
+values ("Rainbow");
+
+
+insert into Genre (name) 
+values ("Рок");
+
+insert into Genre (name) 
+values ("Хард-рок");
+
+insert into Genre (name) 
+values ("Метал");
+
+insert into Genre (name) 
+values ("Хеви-метал");
+
+insert into Genre (name) 
+values ("Экспериментальная");
+
+
+insert into Album (name,date) 
+values ("Rising",1991);
+
+insert into Album (name,date) 
+values ("The house of blue light",2001);
+
+insert into Album (name,date) 
+values ("Stormbringer",2001);
+
+insert into Album (name,date) 
+values ("Animals",1987);
+
+insert into Album (name,date) 
+values ("Life after death",1998);
+
+insert into Album (name,date) 
+values ("13",2005);
+
+insert into Album (name,date) 
+values ("Master of reality",1990);
+
+
+insert into Collection (name,date) 
+values ("Cool-rock",1990);
+
+insert into Collection (name,date) 
+values ("Grillfest",2003);
+
+insert into Collection (name,date) 
+values ("Rock workout",2005);
+
+insert into Collection (name,date) 
+values ("The wall on the storm",1989);
+
+insert into Collection (name,date) 
+values ("100 greatest metal",1988); 
+
+insert into Collection (name,date) 
+values ("Metalheadz",2019);
+
+
